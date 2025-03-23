2cd4b7686cf2f4624b74b77700b3bd26333678b1
Jónathan <jonathan@unionavatars.com>
2025-03-23 16:08:39 +0100

    Adjusted margins

--- index.js
+++ index.js
@@ -28,3 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 })
+
+const appHeight = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
+window.addEventListener('resize', appHeight)
+appHeight()
